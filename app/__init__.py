c95b8686541907ea96d7acc2db638b2ebd713385
Stanley Chin <stanleychin87@gmail.com>
2024-01-17 20:47:31 -0500

    Updated book DataFrame

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+import pandas
 import joblib
 
 app = Flask(__name__)
 CORS(app, resources={"*": {"origins": "http://localhost:4200"}})
 model = joblib.load('model.pkl')
+books = pandas.read_pickle('books')
+ratings = pandas.read_pickle('ratings')
+matrix = pandas.read_pickle('matrix')
 
 from app import routes
